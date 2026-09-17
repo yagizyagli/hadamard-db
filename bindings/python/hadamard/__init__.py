b83505ef7a740fea8fc77c742d68d11114dc5e2c
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-09-18 01:22:18 +0300

    Create __init__.py

---
+++ bindings/python/hadamard/__init__.py
@@ -0,0 +1,9 @@
+"""
+Hadamard-DB: Enterprise-grade, ultra-lightweight quantum database query engine.
+Copyright 2026 Yağız Yağlı. Licensed under Apache-2.0.
+"""
+
+from .client import HadamardClient
+
+__version__ = "0.1.0"
+__all__ = ["HadamardClient"]
