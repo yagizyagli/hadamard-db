20c12c546614b9d2bdf3a9777a532b9e7dcba590
Yağız Yağlı <yagizyagli3447@gmail.com>
2026-09-19 16:56:03 +0300

    Update __init__.py

--- bindings/python/hadamard/__init__.py
+++ bindings/python/hadamard/__init__.py
@@ -1,7 +1,15 @@
-"""
-Hadamard-DB: Enterprise-grade, ultra-lightweight quantum database query engine.
-Copyright 2026 Yağız Yağlı. Licensed under Apache-2.0.
-"""
+import sys
+import os
+
+# Resolve the absolute workspace root workspace layer directly from this package init file
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+WORKSPACE_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, "../../../"))
+
+# Forcefully append compiler build targets straight into python root search path vectors
+sys.path.insert(0, os.path.join(WORKSPACE_ROOT, "target/release/maturin"))
+sys.path.insert(0, os.path.join(WORKSPACE_ROOT, "target/release"))
+sys.path.insert(0, os.path.join(WORKSPACE_ROOT, "core/target/release"))
+sys.path.insert(0, os.path.join(WORKSPACE_ROOT, "bindings/python"))
 
 from .client import HadamardClient
 
